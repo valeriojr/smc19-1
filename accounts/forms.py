6ce5067d03ba0532d476d3f6bb47036e8de1f25b
Valério Nogueira Rodrigues Júnior <vnrj@ic.ufal.br>
2020-03-25 12:17:41 -0300

    Validação de CPF e correção de bug na página de cadastro

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import inlineformset_factory
 
 from . import models
@@ -27,6 +28,35 @@ class AccountCreationForm(forms.ModelForm):
             self.add_error('confirm_password', 'As senhas não coincidem')
 
         return cleaned_data
+
+    def clean_cpf(self):
+        cpf = self.cleaned_data['cpf']
+
+        # verificar o tamanho da string
+        if len(cpf) != 11:
+            raise ValidationError('O CPF deve conter exatamente 11 dígitos', code='invalid')
+
+        # cpfs inválidos
+        for i in range(1, 10):
+            if cpf == str(i) * 11:
+                raise ValidationError('CPF inválido', code='incorrect')
+
+        # validação do primeiro dígito verificador
+        s = 0
+        for i in range(9):
+            s += int(cpf[i]) * (10 - i)
+        first_digit_valid = int(cpf[9]) == ((s * 10 % 11) % 10)
+
+        # validação do segundo dígito verificador
+        s = 0
+        for i in range(10):
+            s += int(cpf[i]) * (11 - i)
+        second_digit_valid = int(cpf[10]) == ((s * 10 % 11) % 10)
+
+        if first_digit_valid and second_digit_valid:
+            return cpf
+
+        raise ValidationError('CPF inválido', code='invalid')
 
 
 class ProfileCreationForm(forms.ModelForm):
--- smc19/settings.py
+++ smc19/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'afr)&4ipm5wp2_-9c=no-!a+tq&frk3lgfze$t#+%ak#n*=+%z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'smc19.herokuapp.com',
@@ -132,13 +132,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'staticfiles'),
+    )
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 LOGIN_REDIRECT_URL = reverse_lazy('monitoring:home')
