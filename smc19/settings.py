6a1ddc86d93401bce02dfaca9deafeba0976f566
Valério Nogueira Rodrigues Júnior <vnrj@ic.ufal.br>
2020-03-28 22:57:36 -0300

    STATIC_URL no servidor https://adrianobarbosa.xyz/smc19-static/

--- smc19/settings.py
+++ smc19/settings.py
@@ -132,12 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+    STATIC_URL = '/static/'
+else:
+    STATIC_URL = 'https://adrianobarbosa.xyz/smc19-static/'
 
 # Extra places for collectstatic to find static files.
 
