70cfe3c11badc22bc1aaccac5793011692c69654
juuze <jass2@ic.ufal.br>
2020-03-29 23:38:41 -0300

    Falta logica de filtrar os dados.

--- evolution/views.py
+++ evolution/views.py
@@ -4,12 +4,34 @@ from django.views import View
 from django.contrib.auth import mixins
 
 import csv
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from monitoring.models import State,City,Neighbourhood
 
 
 class DataGraphEvolution(mixins.LoginRequiredMixin, View):
     def get(self, request):
-        print(request.GET)
+        state = request.GET.get('UF','')
+        city = request.GET.get('CIDADE','')
+        neighbourhood = request.GET.get('BAIRRO','')
+        status = request.GET.get('STATUS','')
+
+        TODOS = [{'value':'TODOS', 'text':'TODOS'}]
+
+        if request.GET.get('UF','') == 'ELEMENTOS':
+            FETCHED = [{'value':u.name, 'text': u.name} for u in State.objects.all()]
+            return JsonResponse(TODOS + FETCHED, safe=False)
+
+        if request.GET.get('CIDADE','') == 'ELEMENTOS':
+            state = State.objects.get(name=request.GET.get('UF',''))
+            FETCHED = [{'value':u.name, 'text': u.name} for u in City.objects.filter(state=state)]
+            return JsonResponse(TODOS + FETCHED, safe=False)
+
+        if request.GET.get('BAIRRO','') == 'ELEMENTOS':
+            state = State.objects.get(name=request.GET.get('UF',''))
+            city = City.objects.get(state=state, name=request.GET.get('CIDADE',''))
+            FETCHED = [{'value':u.name, 'text': u.name} for u in Neighbourhood.objects.filter(city=city)]
+            return JsonResponse(TODOS + FETCHED, safe=False)
 
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="dataset.csv"'
