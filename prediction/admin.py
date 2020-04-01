a3babff869833824dceeb8ec52cd52e3fd8cf66c
hugotallys <htmo@ic.ufal.br>
2020-03-31 22:09:20 -0300

    implementando modelo de status da unidade e atualizando lista de unidades no banco

--- prediction/admin.py
+++ prediction/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import HealthCenter
+from .models import HealthCenter, HealthCenterStatus
 
 admin.site.register(HealthCenter)
+admin.site.register(HealthCenterStatus)
--- prediction/models.py
+++ prediction/models.py
@@ -5,21 +5,29 @@ from django.db import models
 class HealthCenter(models.Model):
     center_name = models.CharField(verbose_name='Nome da Unidade:', max_length=100)
 
-    beds = models.PositiveIntegerField(verbose_name='Leitos total', default=0)
-    occupied_beds = models.PositiveIntegerField(verbose_name='Leitos ocupados', default=0)
-
-    icus = models.PositiveIntegerField(verbose_name='UTIs total', default=0)
-    occupied_icus = models.PositiveIntegerField(verbose_name='UTIs ocupadas', default=0)
-
-    respirators = models.PositiveIntegerField(verbose_name='Respiradores total', default=0)
-    occupied_respirators = models.PositiveIntegerField(verbose_name='Respiradores ocupados', default=0)
-
-    necessary_beds = models.PositiveIntegerField(verbose_name='Leitos necessários', default=0)
-    necessary_icus = models.PositiveIntegerField(verbose_name='UTIs necessárias', default=0)
-    necessary_respirators = models.PositiveIntegerField(verbose_name='UTIs necessárias', default=0)
-
     latitude = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(+90)])
     longitude = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(+180)])
 
     def __str__(self):
         return self.center_name
+
+class HealthCenterStatus(models.Model):
+    health_center = models.ForeignKey(HealthCenter, on_delete=models.CASCADE)
+    
+    date = models.DateTimeField(verbose_name='Data da atualização')
+    
+    beds = models.PositiveIntegerField(verbose_name='Leitos RET total', blank=False)
+    occupied_beds = models.PositiveIntegerField(verbose_name='Leitos RET ocupados', blank=False)
+
+    icus = models.PositiveIntegerField(verbose_name='Leitos UTIs total', blank=False)
+    occupied_icus = models.PositiveIntegerField(verbose_name='Leitos UTIs ocupados', blank=False)
+
+    respirators = models.PositiveIntegerField(verbose_name='Respiradores total', blank=False)
+    occupied_respirators = models.PositiveIntegerField(verbose_name='Respiradores ocupados', blank=False)
+
+    necessary_beds = models.PositiveIntegerField(verbose_name='Leitos RET necessários', blank=False)
+    necessary_icus = models.PositiveIntegerField(verbose_name='Leitos UTIs necessários', blank=False)
+    necessary_respirators = models.PositiveIntegerField(verbose_name='Respiradores necessários', blank=False)
+
+    def __str__(self):
+        return 'Status da unidade ' + str(self.health_center.center_name) + ' em ' + str(self.date) 
