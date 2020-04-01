da70c85fbeaa923f61a4354041c9ac69e23091ab
Samuel <sls@ic.ufal.br>
2020-04-01 14:31:49 -0300

    Criação de usuários finalizada completamente. Agora falta apenas adaptar a visualização da criação para cada tipo de usuário logado. Será implemtado ainda as permissões de cada tipo de usuário. #3

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django import forms
 from django.forms import inlineformset_factory
 from django.utils import timezone
-from django.forms.widgets import RadioSelect
+from django.forms.widgets import RadioSelect, Select
 from accounts import choices
 from . import models
 
@@ -15,10 +15,10 @@ class AccountCreationForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput(),
             'user_profile': RadioSelect,
-            'health_center': RadioSelect
+            'health_center': Select
         }
         help_texts = {
-            'password': 'Inserir critérios de validação',
+            'password': 'A senha deve ter no mínimo 8 caracteres.\nA senha não pode ser igual ao cpf.\nA senha não pode ser inteiramente númerica.\nA senha não pode ser muito comum.',
             'cpf': 'Apenas números',
             'user_profile': 'Escolha um item da lista'
         }
@@ -27,12 +27,21 @@ class AccountCreationForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super(AccountCreationForm, self).clean()
+
+        #checa se todas as chaves existem
+        #cleaned_data só possuem os campos válidos.
+        for key in ['password', 'confirm_password', 'health_center', 'user_profile']:
+            if key not in cleaned_data:
+                return cleaned_data
+        print(dir(self))
         password = cleaned_data['password']
         confirm_password = cleaned_data['confirm_password']
+        health_center = cleaned_data['health_center']
         user_profile = cleaned_data['user_profile']
 
         if password != confirm_password:
             self.add_error('confirm_password', 'As senhas não coincidem')
-
+        if (user_profile == 'AU' or user_profile == 'AD') and (health_center is None):
+            self.add_error('health_center', 'Você deve escolher uma unidade de saúde.')
         return cleaned_data
 
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic
-
+from django.shortcuts import  redirect
 from . import forms
 
 
@@ -17,5 +17,17 @@ class SignUp(generic.CreateView):
         messages.error(self.request, form.errors)
         return super(SignUp, self).form_invalid(form)
 
-    def get_success_url(self):
-        return self.request.GET.get('next', reverse('login'))
+    def form_valid(self, form):
+        #Aqui é feita outras validações que não são possíveis no form.clean()
+        user = self.request.user
+        user_profile = form.cleaned_data['user_profile']
+        health_center = form.cleaned_data['health_center']
+        if user.user_profile == 'AU':#atendente de uma unidade tentou criar um usuário
+            messages.error(self.request, "Você não tem permissão para criar outros usuários.")
+        elif (user.user_profile == 'AD') and (user_profile == 'SS'):#tentou criar um perfil Secretaria de saude
+            messages.error(self.request, "Você não pode criar esse tipo de usuário.")
+        elif (user.user_profile == 'AD') and (health_center != user.health_center):#tentou criar usuário de outra unidade
+            messages.error(self.request, "Você não pode criar usuários de outras unidades.")
+        else:
+            messages.success(self.request, "Usuário criado com sucesso.")
+        return redirect('accounts:sign-up')
